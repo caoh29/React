9025f4e1cc27873ef3fec2d660e5f6e5fcf6fddd
Camilo Ordoñez <cronox20@gmail.com>
2023-07-17 18:06:17 -0500

    adding onclick event handler

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import './slider.css';
 
 const PLANETS = [
   'Mercury',
@@ -12,11 +11,25 @@ const PLANETS = [
   'Neptune'
 ];
 
+const changeTheme = () => {
+  const body = document.querySelector('body');
+  const allLiElements = document.querySelectorAll('li');
+  const isDarkMode = body.classList.contains('dark');
+
+  if (isDarkMode) {
+    body.classList.remove('dark');
+    allLiElements.forEach(li => li.style.color = 'black');
+  } else {
+    body.classList.add('dark');
+    allLiElements.forEach(li => li.style.color = '#66bb6a');
+  }
+};
+
 function App() {
   return (
     <div className="App">
       <label className="switch" htmlFor="checkbox">
-        <input type="checkbox" id="checkbox" />
+        <input type="checkbox" id="checkbox" onClick={() => changeTheme()}/>
         <div className="slider round"></div>
       </label>
       <h1 style={{ color: '#999', fontSize: '19px' }}>Solar system planets:</h1>
