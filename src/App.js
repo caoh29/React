389609670534349e10a4b8309b1f909ed1b88a1d
Camilo Ordoñez <cronox20@gmail.com>
2023-07-17 17:08:27 -0500

    adding title, list and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+const PLANETS = [
+  'Mercury',
+  'Venus',
+  'Earth',
+  'Mars',
+  'Jupiter',
+  'Saturn',
+  'Uranus',
+  'Neptune'
+];
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1 className='title'>Solar system planets:</h1>
+      <ul className='planets-list'>
+        {
+          PLANETS.map((planet, index) => <li key={index}>{planet}</li>)
+        }
+      </ul>
     </div>
   );
 }
